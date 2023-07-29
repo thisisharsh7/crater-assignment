22e333f2a0ad3f95c412fe807e14c67d68787645
Harsh Kumar <9u.harsh@gmail.com>
2023-07-29 12:38:14 +0530

    platform design done

--- src/Components/Platforms.jsx
+++ src/Components/Platforms.jsx
@@ -2,10 +2,10 @@ import { Icon } from "@iconify/react";
 
 export default function Platform() {
     return (
-        <div className="flex px-20 text-white py-20 gap-20">
-            <div className="flex flex-col flex-1 pt-10 gap-12 items-center">
-                <h1 className="text-[38px] leading-tight font-medium">Get all your Platform Data. All in one place.</h1>
-                <ul className="flex gap-12 text-[22px] justify-around w-full items-center font-bold">
+        <div className="flex text-white xl:px-20 lg:px-16 md:px-10 px-6  lg:py-20 md:py-16 py-12 lg:gap-20 gap-8">
+            <div className="flex flex-col flex-1 pt-5 gap-12 items-center text-center">
+                <h1 className="lg:text-[38px] md:text-[34px] text-[27px] leading-tight font-medium">Get all your Platform Data. All in one place.</h1>
+                <ul className="flex sm:flex-row flex-col lg:gap-12  md:gap-8 lg:text-[22px] md:text-[20px] text-[19px] gap-6 justify-around w-full items-center font-bold">
                     <li className="flex items-center gap-3">
                         <Icon icon="logos:youtube-icon" />
                         <a href="/" >Yoututbe</a>
