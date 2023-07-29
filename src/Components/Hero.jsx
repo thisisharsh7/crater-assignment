1a8637e0a6fde29f4f9fb8e5466c2ce831d5b79f
Harsh Kumar <9u.harsh@gmail.com>
2023-07-29 12:25:44 +0530

    hero design done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,9 @@
 export default function Hero() {
     return (
 
-        <div className="flex px-20 text-white py-20 gap-20">
-            <div className="flex flex-col flex-1 items-start py-10 relative gap-4">
-                <h1 className="text-[54px] leading-tight font-bold  before:absolute before:top-0 before:bg-white before:w-20 before:h-0.5">AI-powered Business Manager for
+        <div className="flex lg:flex-row flex-col xl:px-20 lg:px-16 md:px-10 px-6  lg:py-20 md:py-16 py-12 lg:gap-20 gap-8 text-white">
+            <div className="flex flex-col flex-1 items-start lg:py-14 md:py-10 py-5 relative gap-4 xl:text-[10px] lg:text-[6.5px] md:text-[8px] text-[5.3px]">
+                <h1 className="text-[5.4em] leading-tight font-bold  before:absolute before:top-0 before:bg-white md:before:w-20 before:w-8 before:h-0.5">AI-powered Business Manager for
                     {/* <span>Rebels</span>
                     <span>Creators</span> */}
                     <span> Freelancers</span>
@@ -12,13 +12,13 @@ export default function Hero() {
                     <span>Leaders</span>
                     <span>Challengers</span> */}
                 </h1>
-                <p className="text-[20px] text-[#d7d7d7] pb-10">Focus on doing what you love, instead of having to open 10 different social media platforms, negotiate brand deals, chase invoices, manage payments, or file taxes.</p>
+                <p className="sm:text-[2em] text-[13px] text-[#d7d7d7] sm:pb-10 pb-6">Focus on doing what you love, instead of having to open 10 different social media platforms, negotiate brand deals, chase invoices, manage payments, or file taxes.</p>
                 {/* <p>It&apos;s like having your own <span>BUSINESS MANAGER !</span></p> */}
-                <button className="hover:bg-white hover:text-[#0f1711] text-white transition-all duration-500 font-semibold tracking-wider py-3 px-4  hover:border-[#0f1711]  border-2 ">
+                <button className="hover:bg-white sm:text-[1.8em] text-[12px] hover:text-[#0f1711] text-white transition-all duration-500 font-semibold tracking-wider py-3 px-4  hover:border-[#0f1711]  border-2 ">
                     <a href="#get-early-access ">GET EARLY ACCESS</a>
                 </button>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 self-end">
                 <img src="./hero.png" alt="" className="w-full" />
             </div>
         </div>
